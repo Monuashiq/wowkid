bebf6ba65ceeef87fbb74d39a3f8e29e8d3f2b95
fathima-nihala <fathimanihala200214@gmail.com>
2025-08-16 12:37:24 +0530

    latest

--- src/config/db.js
+++ src/config/db.js
@@ -1,33 +1,4 @@
-// import pkg from "pg";
-// const { Pool } = pkg;
-// import dotenv from "dotenv";
 
-// dotenv.config();
-
-
-// const pool = new Pool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT || 5432,
-//   ssl: {
-//     rejectUnauthorized: false // Required for AWS RDS
-//   }
-// });
-
-// pool.on('connect', () => {
-//   console.log('Connected to PostgreSQL database');
-// });
-
-// pool.on('error', (err) => {
-//   console.error('Database connection error:', err);
-// });
-
-// export default pool;
-
-
-// src/config/db.js - Debug Version
 import { Pool } from "pg";
 
 const pool = new Pool({
